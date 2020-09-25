79adb2aeea2290e58dfde3dc3ed63185c097a4d9
hgs13 <hgs13@duke.edu>
2020-09-25 09:34:47 -0400

    initial commite of ARGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Hannah Smith (hgs13@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------------------------
+
